533443f56e4b15d78adf01b1772f5031ebb433e0
Susant Sahani <ssahani@redhat.com>
2026-03-29 16:55:55 +0530

    Revert "networkd: add support for global blackhole/prohibit/unreachable routes in networkd.conf"
    This reverts commit 54addd5a540469267ac9833419b9050108ba9720.

--- src/network/networkd-conf.c
+++ src/network/networkd-conf.c
@@ -7,7 +7,6 @@
 #include "networkd-address-label.h"
 #include "networkd-conf.h"
 #include "networkd-manager.h"
-#include "networkd-route.h"
 #include "networkd-speed-meter.h"
 
 int manager_parse_config_file(Manager *m) {
@@ -20,7 +19,6 @@ int manager_parse_config_file(Manager *m) {
                         "Network\0"
                         "IPv6AcceptRA\0"
                         "IPv6AddressLabel\0"
-                        "Route\0"
                         "DHCPv4\0"
                         "DHCPv6\0"
                         "DHCPServer\0"
@@ -38,7 +36,6 @@ int manager_parse_config_file(Manager *m) {
         }
 
         manager_drop_invalid_address_labels(m);
-        manager_drop_invalid_routes(m);
 
         return 0;
 }
--- src/network/networkd-manager.c
+++ src/network/networkd-manager.c
@@ -764,7 +764,6 @@ Manager* manager_free(Manager *m) {
         m->nexthop_ids = set_free(m->nexthop_ids);
 
         m->address_labels_by_section = hashmap_free(m->address_labels_by_section);
-        m->routes_by_section = hashmap_free(m->routes_by_section);
 
         sd_event_source_unref(m->speed_meter_event_source);
         sd_event_unref(m->event);
@@ -812,10 +811,6 @@ int manager_start(Manager *m) {
         manager_set_sysctl(m);
 
         r = manager_request_static_address_labels(m);
-        if (r < 0)
-                return r;
-
-        r = manager_request_static_routes(m);
         if (r < 0)
                 return r;
 
